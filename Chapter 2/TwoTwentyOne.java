a20441d0d7ac4d1ff71cabb909d304f1949fdfec
Eniifeadeniyi <eniifeoluwaadeniyi16@gmail.com>
2025-09-22 05:35:54 +0100

    Add files via upload

---
+++ Chapter 2/TwoTwentyOne.java
@@ -0,0 +1,11 @@
+public class TwoTwentyOne {
+public static void main(String[] args) {
+
+System.out.print("*");
+System.out.print("***");
+System.out.print("*****");
+System.out.print("****");
+System.out.println("**");
+
+}
+}
