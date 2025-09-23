b5e27c741ea2809de6a0e93763776d380bc59266
eniife <eniifeoluwaadeniyi16@gmail.com>
2025-09-23 23:17:02 +0100

    Added LetterChecker.java

--- Extra-Tasks/LetterChecker.java
+++ Extra-Tasks/LetterChecker.java
@@ -1,1 +1,36 @@
-"Hello Git" 
+import java.util.Scanner;
+
+public class LetterChecker {
+public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+
+System.out.print("Enter a letter: ");
+String letter = input.nextLine().toLowerCase();
+
+if (letter.equals("a")) {
+System.out.print(letter + " is a vowel");
+}
+
+else if (letter.equals("e")) {
+System.out.print(letter + " is a vowel");
+}
+
+else if (letter.equals("i")) {
+System.out.print(letter + " is a vowel");
+}
+
+else if (letter.equals("o")) {
+System.out.print(letter + " is a vowel");
+}
+
+else if (letter.equals("u")) {
+System.out.print(letter + " is a vowel");
+}
+
+else {
+System.out.print(letter + " is a consonant");
+}
+
+
+}
+}
