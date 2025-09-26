9d162e28aa4f8724f75494f9f2cc70943bdf5434
eniife <eniifeoluwaadeniyi16@gmail.com>
2025-09-26 11:59:28 +0100

    Added Chapter-4 with GasMileage.java

---
+++ Chapter-4/FourSeven.java
@@ -0,0 +1,12 @@
+public class FourSeven {
+public static void main(String[] args) {
+
+
+int x = 5;
+int product = x * x++;
+
+System.out.println(+ product);
+System.out.print(+ x++);
+
+}
+}
---
+++ Chapter-4/FourSix.java
@@ -0,0 +1,13 @@
+public class FourSix {
+public static void main(String[] args) {
+
+int sum = 0;
+int x = 1;
+
+while(x <= 10) {
+sum = sum + x;
+x += 1;
+}
+System.out.print("The sum is: " + sum);
+}
+}
