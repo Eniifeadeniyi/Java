fa9145515ff93cdaaa31fd487f57ef7b64a018fe
Eniifeadeniyi <eniifeoluwaadeniyi16@gmail.com>
2025-09-28 19:18:34 +0100

    Create WorldPopulationGrowth.java

---
+++ Chapter-4/WorldPopulationGrowth.java
@@ -0,0 +1,21 @@
+public class PopulationGrowth {
+public static void main(String[] args) {
+double population = 8231613070.0;
+double growthRate = 0.0085;
+double twicePopulation = population * 2;
+int year = 1;
+
+System.out.println("Year\tPopulation\t\tIncrease");
+while (year <= 75) {
+double newPopulation = population * Math.pow(1 + growthRate, year);
+double increase = newPopulation - population;
+
+System.out.println(year + "\t" + newPopulation + "\t" + increase);
+
+if (newPopulation >= twicePopulation) {
+System.out.println("By year " + year + ", the population will be twice the current population");
+}
+year++;
+}
+}
+}
