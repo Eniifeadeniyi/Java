2b0d1cb78bbb0f2d9ee1d4423fcc403382e7d3c3
Eniifeadeniyi <eniifeoluwaadeniyi16@gmail.com>
2025-09-29 12:45:56 +0100

    Update CreditLimitCalculator.java

--- Chapter-4/CreditLimitCalculator.java
+++ Chapter-4/CreditLimitCalculator.java
@@ -5,20 +5,20 @@ public static void main(String[] args) {
 Scanner input = new Scanner(System.in);
 
 System.out.print("Enter your account number: ");
-long accountnumber = input.nextInt();
+int accountnumber = input.nextInt();
 
 System.out.print("Enter your initial balance: ");
-long initialbalance = input.nextInt();
+int initialbalance = input.nextInt();
 
 System.out.print("Enter total of all items charged by customer this month: ");
 int items = input.nextInt();
 
 System.out.print("Enter total of all credits applied to the customers account this month: ");
-long total = input.nextInt();
+int total = input.nextInt();
 
 System.out.print("Enter credit limit: ");
 int creditlimit = input.nextInt();
-long newbalance = initialbalance + (items - total);
+int newbalance = initialbalance + (items - total);
 
 System.out.print(newbalance);
 
@@ -28,4 +28,5 @@ System.out.print("Credit limit exceeded!");
 
 
 }
+
 }
