d680b9dbe84a90c8d340aa2f45c9d4c0eeff7ae8
Eniifeadeniyi <eniifeoluwaadeniyi16@gmail.com>
2025-09-29 01:30:38 +0100

    Update PrimeNumber.java

--- Gate-One/PrimeNumber.java
+++ Gate-One/PrimeNumber.java
@@ -7,7 +7,18 @@ Scanner input = new Scanner(System.in);
 System.out.print("Enter a positive number: ");
 int number = input.nextInt();
 
-
-
+int divisor = 2;
+while(divisor <= number) {
+  if (number % divisor == 0) {
+    System.out.print(number + " is not a prime number");
+      }
+    divisor++;
+    if(number % divisor != 0) {
+      System.out.print(number + " is a prime number");
+      break;
+       
+    }
+    }
+}
 }
 }
