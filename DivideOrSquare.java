af8304536642d4f75051136e824ffde2659ff776
eniife <eniifeoluwaadeniyi16@gmail.com>
2025-10-03 08:54:29 +0100

    Added DivideOrSquare.java

---
+++ DivideOrSquare.java
@@ -0,0 +1,15 @@
+public class DivideOrSquare {
+public static void main(String[] args) {
+
+System.out.print(divideorsquare(10));
+}
+
+public static double divideorsquare(double number) {
+	if(number % 5 == 0) {		
+		return Math.pow(number * 1.0, 0.5);
+	if(number % 5 != 0) {	
+		return (number * 1.0) % 5; }
+}
+
+}
+}
