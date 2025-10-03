3c410d1481f58d7ace54f426ccfd3bf0474e11ee
eniife <eniifeoluwaadeniyi16@gmail.com>
2025-10-03 13:58:49 +0100

    Added PrimeNumber.java

--- Gate-One/PrimeNumber.java
+++ Gate-One/PrimeNumber.java
@@ -4,21 +4,27 @@ public class PrimeNumber {
 public static void main(String[] args) {
 Scanner input = new Scanner(System.in);
 
-System.out.print("Enter a positive number: ");
+System.out.print("Enter a number: ");
 int number = input.nextInt();
 
-int divisor = 2;
-while(divisor < number) {
-  if (number % divisor == 0) {
-    System.out.print(number + " is not a prime number");
-    break;
-      }
-    divisor++;
-    if(number % divisor != 0) {
-      System.out.print(number + " is a prime number");
-       
-    }
-    }
+int divisor = 1;
+int factorCount = 0;
+
+while(divisor <= number) {
+int divide = number % divisor;
+
+if(divide == 0){
+factorCount += 1;
 }
+divisor += 1;
+}
+
+if(factorCount == 2) {
+System.out.print(number + " is a prime number");
+}
+if(factorCount > 2) {
+System.out.print(number + " is not a prime number."); 
+}
+ 
 }
 }
