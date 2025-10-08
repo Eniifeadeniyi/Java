f8e06728617576bcedc43b2f8383c8410ca102db
eniife <eniifeoluwaadeniyi16@gmail.com>
2025-10-08 21:11:10 +0100

    Add FirstElement.java

---
+++ Cupcakes/FirstElement.java
@@ -0,0 +1,10 @@
+public class FirstElement {
+public static void main(String[] args) {
+
+System.out.print(getFirstElement(new int [] {125,25,67,89}));
+}
+public static int getFirstElement(int[] numbers) {
+	return numbers[0];
+
+}
+}
