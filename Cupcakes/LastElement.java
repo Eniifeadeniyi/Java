d4007fe77e1b76c80a38563a06d93b6740ad26d6
Eniifeadeniyi <eniifeoluwaadeniyi16@gmail.com>
2025-10-09 03:37:31 +0100

    Update LastElement.java

--- Cupcakes/LastElement.java
+++ Cupcakes/LastElement.java
@@ -4,6 +4,9 @@ public static void main(String[] args) {
 System.out.print(getLastElement(new int [] {2,4,6,8,10}));
 }
 public static int getLastElement(int[] numbers) {
+	if(numbers.length == 0) {
+		return 0;
+	}
 	return numbers[numbers.length - 1];
 
 
