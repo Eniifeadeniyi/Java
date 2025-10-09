1145549b729bb5e102cd4ce7f628302f5a9f33dd
Eniifeadeniyi <eniifeoluwaadeniyi16@gmail.com>
2025-10-09 20:42:49 +0100

    Update Swap.java

--- Cupcakes/Swap.java
+++ Cupcakes/Swap.java
@@ -9,12 +9,15 @@ public static void swapFirstAndLast(int[] numbers) {
 	result[0] = numbers[numbers.length - 1];
 	System.out.print(result[0]);
 
+		
+ 	for(int count = 1; count < numbers.length - 1; count ++) {
+		result[count] = numbers[count];
+		System.out.print(result[count]);
+
+	}
+
 	result[numbers.length - 1] = numbers[0];
 	System.out.print(result[numbers.length - 1]);
 
-	for(int count = 1; count < numbers.length - 1; count ++) {
-		result[count] = numbers[count];
-		System.out.print(result[count]);
-		}
 }
 }
