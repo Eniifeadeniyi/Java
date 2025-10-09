70fa025c4b68b26891d2dbb51a4b800f7b8455d5
Eniifeadeniyi <eniifeoluwaadeniyi16@gmail.com>
2025-10-09 03:38:22 +0100

    Update CountOccurrence.java

--- Cupcakes/CountOccurrence.java
+++ Cupcakes/CountOccurrence.java
@@ -6,7 +6,7 @@ System.out.print(countOccurrence(12, new int [] {13,14,12,12,12,15,12,20,12,12})
 
 public static int countOccurrence(int target, int[] numbers) {
 	int targetCount = 0;
-	for(int count = 0; count < 10; count++) {
+	for(int count = 0; count < numbers.length; count++) {
 		if(numbers[count] == target) {
 			targetCount += 1;
 	}
